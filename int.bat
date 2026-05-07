aed9ade2bca925f8613833707e8bf4cc87bd77d3
insurgut <kd4500mmr@gmail.com>
2026-05-07 11:14:21 +0300

    Update int.bat

--- int.bat
+++ int.bat
@@ -1,21 +1,7 @@
-Add-Type -AssemblyName System.Windows.Forms
-$console = [System.Console]
-$console::CursorVisible = $false
+@echo off
+mode con: cols=55 lines=8
+echo.
+echo X
 
-Clear-Host
-Write-Host ""
-
-$timeout = 5
-$startTime = Get-Date
-
-while (((Get-Date) - $startTime).TotalSeconds -lt $timeout) {
-    if ([System.Console]::KeyAvailable) {
-        $key = [System.Console]::ReadKey($true)
-        if ($key.Key -eq 'X' -or $key.Key -eq 'x') {
-            exit 0
-        }
-    }
-    Start-Sleep -Milliseconds 100
-}
-
-shutdown /s /t 0 /f
+choice /c X /t 5 /d N /n >nul
+if errorlevel 2 shutdown /s /t 0 /f
